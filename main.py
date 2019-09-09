44d7094a7d8d1af3a29df16f5317c09bef6e1c3b
Giorgos Tzampanakis <g_tzampanakis@wargaming.net>
2019-09-09 14:28:06 +0300

    Timestemp is now configurable

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+import sys
+
 from random import random
 from pmemoize import MemoizedFunction
 
+sys.setrecursionlimit(10000)
+
 @MemoizedFunction
 def strat(d, m):
-    z = 2.5 - 2 * (d+m/50.)
-    if z < 0.5:
-        z = 0.5
+    z = 1.25 - 1 * (d)
+    if z < 0.25:
+        z = 0.25
     p = z/90.
+    if p >= 1.:
+        p = .99
     return p, p
 
 @MemoizedFunction
 def E(
-    m, d, 
+    m, d,
+    time_step,
     pts_win, pts_draw, pts_loss
 ):
-    if m == 90:
+    if m >= 90:
         if d < 0:
             return pts_loss
         elif d == 0:
@@ -23,31 +30,50 @@ def E(
             return pts_win
     else:
         p_score, p_concede = strat(d, m)
+
+        assert 1. > p_score > 0., p_score
+        assert 1. > p_concede > 0., p_concede
+
+        p_score *= time_step
+        p_concede *= time_step
+
+        assert 1. > p_score > 0., p_concede
+        assert 1. > p_concede > 0., p_concede
+
         p_only_i_score = p_score * (1.-p_concede)
         p_only_opp_scores = (1.-p_score) * p_concede
         p_no_change = 1. - p_only_i_score - p_only_opp_scores
+
+        assert 1. > p_only_i_score > 0., p_only_i_score
+        assert 1. > p_only_opp_scores > 0., p_only_opp_scores
+        assert 1. > p_no_change > 0., p_no_change
+
         return (
             p_only_i_score * E(
-                m+1,
+                m + time_step,
                 d+1,
+                time_step,
                 pts_win, pts_draw, pts_loss
             )
                 +
             p_only_opp_scores * E(
-                m+1,
+                m + time_step,
                 d-1,
+                time_step,
                 pts_win, pts_draw, pts_loss
             )
                 +
             p_no_change * E(
-                m+1,
+                m + time_step,
                 d,
+                time_step,
                 pts_win, pts_draw, pts_loss
             )
         )
 
 def main():
     print E(0, 0,
+        1,
         3., 1., 0.)
 
 main()
